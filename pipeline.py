6508f9fe7c07e3f6c90715d25077b46c4315934c
arkiver <arkiver@protonmail.com>
2024-03-22 16:49:58 +0100

    Version 20240323.04. Set multi item size to 1000 to limit on tracker side.

--- pipeline.py
+++ pipeline.py
@@ -72,11 +72,11 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20240323.03'
+VERSION = '20240323.04'
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/116.0'
 TRACKER_ID = 'deviantart'
 TRACKER_HOST = 'legacy-api.arpa.li'
-MULTI_ITEM_SIZE = 20
+MULTI_ITEM_SIZE = 1000
 
 
 ###########################################################################
