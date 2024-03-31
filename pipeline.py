901bafc69c88a23ef12ccce9fa9d001888d22274
arkiver <arkiver@protonmail.com>
2024-03-31 09:49:04 +0200

    Version 20240331.01. Use Firefox Linux user agent.

--- pipeline.py
+++ pipeline.py
@@ -72,8 +72,8 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20240326.02'
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0'
+VERSION = '20240331.01'
+USER_AGENT = 'Mozilla/5.0 (X11; Linux i686; rv:124.0) Gecko/20100101 Firefox/124.0'
 TRACKER_ID = 'deviantart'
 TRACKER_HOST = 'legacy-api.arpa.li'
 MULTI_ITEM_SIZE = 1000
